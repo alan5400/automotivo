69c03664bf6f413bef0ca8dc947ecd3e73222c01
Alan Aanami Ishida <anamialan44@gmail.com>
2024-10-08 11:56:42 -0300

    fgreytyrty

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Automobilismo',
+    'Qual é o carro mais rápido?',
+    'É o SSC Tuatara com 1750 cavalos de potência, motor v8 biturbo de 5,9 litros.'
+)
+
+criaCartao(
+    'Automobilismo',
+    'Qual é o modelo de produção que revolucionou no século 19?',
+    'Fordismo é um modelo produtivo criado por Henry Ford no início do século 19 nos Estados Unidos. O referido sistema foi desenhado para a indústria automobilística com o objetivo de aumentar a produtividade, diminuindo os custos de produção.'
+)
+
+criaCartao(
+    'Automobilismo',
+    'Como é funcionado o modelo Toyotismo na indústria?',
+    'É um modelo de produção industrial japonês conhecido pela adoção do sistema just-in-time, que preza pelo atendimento da demanda e a eliminação dos estoques criado por Taiichi Ohno no final dos anos 70.'
+)
+
+criaCartao(
+    'Automobilismo',
+    'Qual objetivo do Volvismo na industria automobilistica?',
+    'Criado por Emti Chavanmco nos anos 60, o volvismo  tem como principal objetivo a promoção de uma produção flexível baseada na elevada autonomia e qualificação do trabalhador, lógica tradicional de produção industrial.'
+)
